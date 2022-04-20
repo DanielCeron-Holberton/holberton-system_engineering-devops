94e2a3365cb14f0bd376bf25aa2823ba79f742c1
DanielCeron <3403@holbertonschool.com>
2022-04-19 19:46:58 -0500

    [FIX] - Comma fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a new File
 file { 'school':
-  path               =>  '/tmp/school',
-  source_permissions =>  0744,
-  owner              =>  'www-data',
-  group              =>  'www-data',
-  content            =>  'I love Puppet'
+  path    =>  '/tmp/school',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'I love Puppet',
 }
