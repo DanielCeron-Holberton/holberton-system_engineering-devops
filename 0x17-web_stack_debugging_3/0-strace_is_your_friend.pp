d42bb66bf5f746776b73054dc14a2c7aa4fb2f24
Daniel <3403@holbertonschool.com>
2022-05-04 00:06:44 -0500

    [ADD] - Project added

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fix apache server issue                                                  
+
+exec{'fix-phpfile':
+command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+pcath   => '/bin',
+}
+
